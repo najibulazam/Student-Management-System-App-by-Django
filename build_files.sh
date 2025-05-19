5dbe4d9fcd2317cb657284098f6793c2487433ac
Md Najib Ul Azam Mahi <najibul.me@gmail.com>
2025-05-19 18:38:29 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,3 @@
-#  echo "BUILD START"
-#  python3.9 -m pip install -r requirements.txt
-#  python3.9 manage.py collectstatic --noinput --clear
-#  echo "BUILD END"
-
 #!/bin/bash
 echo "BUILD START"
 python3.9 -m ensurepip --upgrade
